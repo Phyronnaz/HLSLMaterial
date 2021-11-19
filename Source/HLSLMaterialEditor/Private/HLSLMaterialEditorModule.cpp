8b86ea6335547965f558ebe9064ca59b8da88fc5
Adam Wolski <adam@wolski.email>
2021-11-19 13:28:15 +0100

    Fix AssetTypeCategories compilation error on UE5
    In UE5 Material and Texture categories got separated

--- Source/HLSLMaterialEditor/Private/HLSLMaterialEditorModule.cpp
+++ Source/HLSLMaterialEditor/Private/HLSLMaterialEditorModule.cpp
@@ -16,9 +16,20 @@ public:
 
 	//~ Begin IAssetTypeActions Interface
 	virtual FText GetName() const override { return INVTEXT("HLSL Material Function Library"); }
-	virtual uint32 GetCategories() override { return EAssetTypeCategories::MaterialsAndTextures; }
 	virtual FColor GetTypeColor() const override { return FColor(0, 175, 255); }
 	virtual UClass* GetSupportedClass() const override { return UHLSLMaterialFunctionLibrary::StaticClass(); }
+	virtual uint32 GetCategories() override
+	{
+#if ENGINE_MAJOR_VERSION >= 5
+		// We want to support both 5.0EA and 5.0 release
+		// As far as I found they have exactly the same version values in defines so we cannot check for that.
+		// 5.0 release added EAssetTypeCategories::Materials but EA still has MaterialsAndTextures category
+		// They both have the same numerical value in the enum so a hacky solution for now is just to use raw value.
+		return 1 << 2;
+#else
+		return EAssetTypeCategories::MaterialsAndTextures;
+#endif
+	}
 
 	virtual bool HasActions(const TArray<UObject*>&InObjects) const override { return true; }
 	virtual void GetActions(const TArray<UObject*>&InObjects, FMenuBuilder & MenuBuilder) override
